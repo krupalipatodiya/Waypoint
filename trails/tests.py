2c42084abb2842b165729bc4cbb6bf92268f7ac3
Krupali Patodiya <krupali.patodiya@gmail.com>
2026-08-18 09:50:04 -0400

    Complete Week 14 tests and documentation

--- trails/tests.py
+++ trails/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from .models import Trail, Park
+from waypoint_core.domain import Distance
+
 from .models import Trail
 
 
@@ -68,3 +71,46 @@ class TrailCatalogTests(TestCase):
 
         self.assertEqual(trails[0].name, "Short Trail")
         self.assertEqual(trails[1].name, "Long Trail")
+
+class Week14HardeningTests(TestCase):
+
+    def setUp(self):
+        self.park = Park.objects.create(
+            name="Test Park",
+            region="Ontario"
+        )
+
+        self.open_trail = Trail.objects.create(
+            name="Open Trail",
+            park=self.park,
+            distance_km=5.00,
+            elevation_gain=100,
+            difficulty="easy",
+            is_open=True,
+        )
+
+        self.closed_trail = Trail.objects.create(
+            name="Closed Trail",
+            park=self.park,
+            distance_km=8.00,
+            elevation_gain=200,
+            difficulty="moderate",
+            is_open=False,
+        )
+
+    def test_catalog_only_shows_open_trails(self):
+        response = self.client.get(reverse("catalog"))
+
+        self.assertContains(response, "Open Trail")
+        self.assertNotContains(response, "Closed Trail")
+
+    def test_missing_trail_returns_404(self):
+        response = self.client.get(
+            reverse("trail_detail", args=[99999])
+        )
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_distance_rejects_negative_value(self):
+        with self.assertRaises(ValueError):
+            Distance(-5, "km")
